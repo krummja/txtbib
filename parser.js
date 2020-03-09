b3a08d60be44c7956295e92c8709590559df12da
Jonathan Crum <crumja@uga.edu>
2020-03-09 12:16:52 -0400

    Working prototype of citekey generator

--- parser.js
+++ parser.js
@@ -13,20 +13,23 @@ const ABBREV_PAREN_AUTHOR_YEAR = new RegExp(/\(\w*.\s[A-Z]\w*\s\d{4}\)/);
 const BIB_LAST_FIRST = new RegExp(/[A-Z]{1}\w*,\s[A-Z]{1}\w*./);
 const NEWLINE = new RegExp(/\w*.\n/);
 
-
 function checkAgainst(arr, rex, bib) {
 
     var results = [];
 
     for (i = 0; i < arr.length; i++) {
+        // Checks against regex supplied in function call
         if (rex.test(arr[i]) == true) {
+            // Checks if positive match is found in BIB dictionary
             if (bib.includes(arr[i])) {
-
-                var match = {
-                    "Author": arr[i],
-                    "Index": i
+                // Checks is BIB item is followed by YEAR in the original text
+                if (YEAR.test(arr[i+1]) == true) {
+                    var match = {
+                        "Author": arr[i],
+                        "Year": arr[i+1],
+                        "Index": i
+                    }
                 }
-               
                 results.push(match);
             } else {
                 continue;
