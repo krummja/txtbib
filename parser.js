9c076806e804aa7f5edb7808a7aacbeb62312667
Jonathan Crum <crumja@uga.edu>
2020-03-09 19:59:15 -0400

    Parser can now handle passing in the citeClass

--- parser.js
+++ parser.js
@@ -4,8 +4,9 @@ var text = fs.readFileSync('./test.txt').toString('utf-8');
 const BIB = ['Crum', 'Smith'];
 
 class BibMatch {
-    constructor(matchData) {
+    constructor(matchData, citeClass="") {
         this.matchData = matchData;
+        this.citeClass = citeClass;
     }
 
     getMatchInfo() {
@@ -13,14 +14,18 @@ class BibMatch {
         var matchOutdex = matchIndex + this.matchData[0].length;
         var author = this.matchData[1];
         var year = this.matchData[2];
-        return [matchIndex, matchOutdex, author, year];
+        var matchInfo = [matchIndex, matchOutdex, author, year];
+
+        return matchInfo;
     }
 
     buildCiteKey() {
+        var citeClass = this.citeClass
         var index = this.getMatchInfo()[0];
         var normalizedAuthor = this.getMatchInfo()[2].toLowerCase();
         var normalizedYear = this.getMatchInfo()[3].replace(/[\(]/, '').replace(/[\)]/, '');
-        var citeKey = "\\citet{" + normalizedAuthor + normalizedYear + "}" + " at index " + index;
+
+        var citeKey = "\\cite"+ citeClass + "{" + normalizedAuthor + normalizedYear + "}" + " at index " + index;
         // Outputs '\citet{author0000} at index i'
 
         return citeKey
@@ -28,17 +33,18 @@ class BibMatch {
 }
 
 function processText(text) {
-    let paren_author_year = /\(([A-Z]\w*)\s(\d{4})\)/g
-    let author_paren_year = /([A-Z]\w*)\s(\(\d{4}\))/g
-    let str = text
+    let paren_author_year = /\(([A-Z]\w*)\s(\d{4})\)/g;
+    let author_paren_year = /([A-Z]\w*)\s(\(\d{4}\))/g;
+    let str = text;
 
-    var match_list = []
+    var match_list = [];
 
     while ((match = author_paren_year.exec(str)) != null) {
-        var bibOutput = new BibMatch(match)
+        var bibOutput = new BibMatch(match, citeClass="p");
 
-        match_list.push(bibOutput.getMatchInfo())
-        console.log(bibOutput.buildCiteKey())
+        match_list.push(bibOutput.getMatchInfo());
+        console.log(bibOutput.getMatchInfo());
+        console.log(bibOutput.buildCiteKey());
     }
 
     return match_list;
