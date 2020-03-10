0df65a0a5970d4755f50e45b5de7bfdd5fd96e13
Jonathan Crum <crumja@uga.edu>
2020-03-09 22:58:43 -0400

    Trying a new approach

--- parser.js
+++ parser.js
@@ -1,8 +1,6 @@
 var fs = require('fs');
 var text = fs.readFileSync('./test.txt').toString('utf-8');
 
-const BIB = ['Crum', 'Smith'];
-
 class BibMatch {
     constructor(matchData, citeClass="") {
         this.matchData = matchData;
@@ -21,33 +19,59 @@ class BibMatch {
 
     buildCiteKey() {
         var citeClass = this.citeClass
-        var index = this.getMatchInfo()[0];
         var normalizedAuthor = this.getMatchInfo()[2].toLowerCase();
         var normalizedYear = this.getMatchInfo()[3].replace(/[\(]/, '').replace(/[\)]/, '');
 
-        var citeKey = "\\cite"+ citeClass + "{" + normalizedAuthor + normalizedYear + "}" + " at index " + index;
-        // Outputs '\citet{author0000} at index i'
+        var citeKey = " \\cite"+ citeClass + "{" + normalizedAuthor + normalizedYear + "} ";
 
-        return citeKey
+        return citeKey;
     }
 }
 
+
+function replaceString(oldS, newS, fullS) {
+    return fullS.split(oldS).join(newS);
+}
+
+
 function processText(text) {
-    let paren_author_year = /\(([A-Z]\w*)\s(\d{4})\)/g;
-    let author_paren_year = /([A-Z]\w*)\s(\(\d{4}\))/g;
-    let str = text;
+    const paren_author_year = /\(([A-Z]\w*)\s(\d{4})\)/g;
+    const author_paren_year = /([A-Z]\w*)\s(\(\d{4}\))/g;
+    var str = text;
 
     var match_list = [];
 
-    while ((match = author_paren_year.exec(str)) != null) {
+    // Author (2020)
+    while ((match = paren_author_year.exec(str)) != null) {
         var bibOutput = new BibMatch(match, citeClass="p");
 
-        match_list.push(bibOutput.getMatchInfo());
-        console.log(bibOutput.getMatchInfo());
-        console.log(bibOutput.buildCiteKey());
+        var citeKey = bibOutput.buildCiteKey();
+        var output = replaceString(match[0], citeKey, text);
+    }
+
+    // (Author 2020)
+    while ((match = author_paren_year.exec(str)) != null) {
+        var bibOutput = new BibMatch(match, citeClass="t");
+
+        var citeKey = bibOutput.buildCiteKey();
+        var output = replaceString(match[0], citeKey, text);
     }
 
     return match_list;
 }
 
-processText(text);
+
+function writeToFile(citeDictionary, text) {
+    var str = text;
+    var dictionary = citeDictionary;
+
+    // fs.writeFile('output.tex', str, (err) => {
+    //     if (err) throw err;
+    //     console.log('The file has been saved.');
+    // })
+
+    var processed = replaceString('Crum', 'Ramchand', str);
+}
+
+var match_list = processText(text);
+writeToFile(match_list, text);
